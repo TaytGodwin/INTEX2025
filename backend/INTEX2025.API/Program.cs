25e35a7bf1a4759f3028adaf7e271638b685b967
masondalton <mcoled@byu.edu>
2025-04-05 13:48:13 -0600

    Added role based authentication class and changes to program.cs

--- backend/INTEX2025.API/Program.cs
+++ backend/INTEX2025.API/Program.cs
@@ -18,8 +18,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+// builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// This code above is replaced by the one below for using roles
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
