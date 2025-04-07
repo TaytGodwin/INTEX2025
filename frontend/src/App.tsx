d4e89518a9d5c0f25f1a667cb7c7f939b9061eb8
Mekeli Hiller <mekelihiller@gmail.com>
2025-04-07 16:56:24 -0600

    Updated some front end things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,37 @@ import './css/App.css';
 import HomePage from './pages/HomePage';
 import Layout from './components/layout/layout';
 import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfUse from './pages/TermsOfUse'
+import TermsOfUse from './pages/TermsOfUse';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import AdminPage from './pages/Admin';
 import { AuthProvider } from './context/AuthContext';
+import AuthorizeView from './components/authentication/AuthorizeView';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-use" element={<TermsOfUse />} />
-          {/* This will match "/menu" "/terms-of-use"*/}
-          <Route path="/menu" element={<HomePage />} />
-          <Route path="*" element={<Navigate to="/menu" />} />{' '}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+
+          {/* Protected routes */}
+          <Route element={<AuthorizeView children={undefined} />}>
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              
+              <Route path="/menu" element={<HomePage />} />
+              <Route path="*" element={<Navigate to="/menu" />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
