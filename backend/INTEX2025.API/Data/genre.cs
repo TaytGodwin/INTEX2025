e453b00e7a10a5ab30af3146b0d30354d6110173
taterg99 <taterg99@gmail.com>
2025-04-07 16:04:06 -0600

    Adjusted models to have gernes

---
+++ backend/INTEX2025.API/Data/genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class genre
+{
+    [Key]
+    public int GenreID { get; set; }
+
+    public string GenreName { get; set; }
+
+    public ICollection<movie_genre> Genres { get; set; }
+}
