4800f3ad5cafcc4574bb5102c9bda8b2d3a13c29
taterg99 <taterg99@gmail.com>
2025-04-07 14:23:31 -0600

    Updated database connections and models

---
+++ backend/INTEX2025.API/Controllers/MovieController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INTEX.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        // Instance of context file
+        private MovieDbContext _movieContext;
+        public MovieController(MovieDbContext temp) => _movieContext = temp; // Set instance
+
+        [HttpGet("AllMovies")] // Get all Movies for admin user to see, but technically any authorized user could do this
+        [Authorize] // Requires users to be logged in
+        public IActionResult GetBooks(int pageSize = 25, int pageNum = 1, string sortBy = "title", [FromQuery] List<string>? categoryTypes = null) // parameters
+        {
+            // IQueryable are built one thing at a time
+            var query = _movieContext.Movies.AsQueryable();
+
+            if (categoryTypes != null && categoryTypes.Any()) // Check if movie categories are not null
+            {
+                query = query.Where(c => categoryTypes.Contains(c.Category)); // Only get project types when they are in the list
+            }
+
+            var AllBooks = query // Narrowed down, filtered list
+                .OrderBy(sortBy) // Uses using System.Linq.Dynamic.Core; to sort by the preference that the user gave
+                .Skip((pageNum - 1) * pageSize) // Skips the page size amount until it gets to the page you are on
+                .Take(pageSize) // Sends how many the user selected
+                .ToList();
+
+            var totalNumBooks = query.Count();
+
+            var TotalObject = new
+            {
+                Books = AllBooks,
+                totalNumBooks
+            };
+
+            return Ok(TotalObject);
+        }
+
+        [HttpGet("GetCategories")]
+        public IActionResult GetCategories()
+        {
+            var AllCategories = _movieContext.Books
+                .Select(b => b.Category)
+                .Distinct() // Get distinc categories from the books table
+                .ToList();
+
+            return Ok(AllCategories);
+        }
+
+        // To add books
+        [HttpPost("AddBook")]
+        public IActionResult AddBook([FromBody] Book newBook) // Frombody means it is coming in the body as json
+        {
+            _movieContext.Books.Add(newBook);
+            _movieContext.SaveChanges();
+
+            return Ok(newBook);
+        }
+
+        // To edit books
+        [HttpPut("UpdateBook/{BookId}")]
+        public IActionResult UpdateBook(int BookId, [FromBody] Book updatedBook)
+        {
+            var existingBook = _movieContext.Books.Find(BookId); // Find the project to edit
+
+            // Update everything
+            existingBook.Title = updatedBook.Title;
+            existingBook.Author = updatedBook.Author;
+            existingBook.Publisher = updatedBook.Publisher;
+            existingBook.ISBN = updatedBook.ISBN;
+            existingBook.Classification = updatedBook.Classification;
+            existingBook.Category = updatedBook.Category;
+            existingBook.PageCount = updatedBook.PageCount;
+            existingBook.Price = updatedBook.Price;
+
+            // Update database with edits
+            _movieContext.Books.Update(existingBook);
+            _movieContext.SaveChanges();
+
+            return Ok(existingBook);
+        }
+
+        // To delete a book
+        [HttpDelete("DeleteBook/{BookId}")]
+        public IActionResult DeleteBook(int BookId)
+        {
+            var book = _movieContext.Books.Find(BookId); // Get the book
+
+            if (book == null)
+            {
+                return NotFound(new { message = "Project not found" });
+            }
+
+            // Save changes to database
+            _movieContext.Books.Remove(book);
+            _movieContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
+{
+    public class MovieController : Controller
+    {
+    }
+}
---
+++ backend/INTEX2025.API/Data/movies_titles.cs
@@ -0,0 +1,143 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INTEX.API.Data
+{
+    public class movies_titles
+    {
+        [Key]
+        [Required]
+        public string show_id { get; set; }
+        [Required]
+        public string type { get; set; }
+        [Required]
+        public string title { get; set; }
+        
+        public string director {  get; set; } // Not required
+        public string cast  { get; set; } // Not required
+        public string countery { get; set; } // Not requried 
+        [Required]
+        public int release_year { get; set; }
+        [Required]
+        public string rating { get; set; }
+        [Required]
+        public string duration { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public bool Action { get; set; }
+        [Required]
+        public bool Adventure { get; set; }
+        [Column("Anime Series International TV Shows")]
+        [Required]
+        public bool AnimeSeriesInternationalTVShows { get; set; }
+
+        [Column("British TV Shows Docuseries International TV Shows")]
+        [Required]
+        public bool BritishTVShowsDocuseriesInternationalTVShows { get; set; }
+
+        [Required]
+        public bool Children { get; set; }
+
+        [Required]
+        public bool Comedies { get; set; }
+
+        [Column("Comedies Dramas International Movies")]
+        [Required]
+        public bool ComediesDramasInternationalMovies { get; set; }
+
+        [Column("Comedies International Movies")]
+        [Required]
+        public bool ComediesInternationalMovies { get; set; }
+
+        [Column("Comedies Romantic Movies")]
+        [Required]
+        public bool ComediesRomanticMovies { get; set; }
+
+        [Column("Crime TV Shows Docuseries")]
+        [Required]
+        public bool CrimeTVShowsDocuseries { get; set; }
+
+        [Required]
+        public bool Documentaries { get; set; }
+
+        [Column("Documentaries International Movies")]
+        [Required]
+        public bool DocumentariesInternationalMovies { get; set; }
+
+        [Required]
+        public bool Docuseries { get; set; }
+
+        [Required]
+        public bool Dramas { get; set; }
+
+        [Column("Dramas International Movies")]
+        [Required]
+        public bool DramasInternationalMovies { get; set; }
+
+        [Column("Dramas Romantic Movies")]
+        [Required]
+        public bool DramasRomanticMovies { get; set; }
+
+        [Column("Family Movies")]
+        [Required]
+        public bool FamilyMovies { get; set; }
+
+        [Required]
+        public bool Fantasy { get; set; }
+
+        [Column("Horror Movies")]
+        [Required]
+        public bool HorrorMovies { get; set; }
+
+        [Column("International Movies Thrillers")]
+        [Required]
+        public bool InternationalMoviesThrillers { get; set; }
+
+        [Column("International TV Shows Romantic TV Shows TV Dramas")]
+        [Required]
+        public bool InternationalTVShowsRomanticTVShowsTVDramas { get; set; }
+
+        [Column("Kids' TV")]
+        [Required]
+        public bool KidsTV { get; set; }
+
+        [Column("Language TV Shows")]
+        [Required]
+        public bool LanguageTVShows { get; set; }
+
+        [Required]
+        public bool Musicals { get; set; }
+
+        [Column("Nature TV")]
+        [Required]
+        public bool NatureTV { get; set; }
+
+        [Column("Reality TV")]
+        [Required]
+        public bool RealityTV { get; set; }
+
+        [Required]
+        public bool Spirituality { get; set; }
+
+        [Column("TV Action")]
+        [Required]
+        public bool TVAction { get; set; }
+
+        [Column("TV Comedies")]
+        [Required]
+        public bool TVComedies { get; set; }
+
+        [Column("TV Dramas")]
+        [Required]
+        public bool TVDramas { get; set; }
+
+        [Column("Talk Shows TV Comedies")]
+        [Required]
+        public bool TalkShowsTVComedies { get; set; }
+
+        [Required]
+        public bool Thrillers { get; set; }
+
+    }
+}
