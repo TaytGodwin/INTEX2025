60000bfd0ddb316521c4ea701cd5c7a9e8b8807a
Mekeli Hiller <mekelihiller@gmail.com>
2025-04-07 21:13:19 -0600

    fixed APP.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import TermsOfUse from './pages/TermsOfUse';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import AdminPage from './pages/Admin';
-import MoviePage from './pages/MoviePage';
 import MovieDetailPage from './pages/MovieDetailPage'
 import { AuthProvider } from './context/AuthContext';
 import AuthorizeView from './components/authentication/AuthorizeView';
@@ -18,21 +17,21 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* Public Routes */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-use" element={<TermsOfUse />} />
+            <Route path="/menu" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/menu" />} />
+          </Route>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          
 
           {/* Protected Routes */}
           <Route element={<AuthorizeView children={undefined} />}>
-            <Route element={<Layout />}>
-              {/* Set the default page to MoviePage */}
-              <Route index element={<MoviePage />} />
-              <Route path="/movies" element={<MoviePage />} />
-              {/* MovieDetailPage for a specific movie */}
-              <Route path="/movies/:movieId" element={<MovieDetailPage />} />
-              {/* Redirect unknown paths to /movies */}
-              <Route path="*" element={<Navigate to="/movies" />} />
-            </Route>
+            <Route path="/movieDetails" element={<MovieDetailPage/>} />
+            <Route path="/admin" element={<AdminPage />} />
+            {/* Add other protected routes here */}
           </Route>
         </Routes>
       </BrowserRouter>
