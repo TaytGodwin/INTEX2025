cbb9959592f4cb756a39e65156c911b512792edd
IcyThunder22 <153141281+IcyThunder22@users.noreply.github.com>
2025-04-08 09:26:09 -0600

    started to work on maping movie images in movie description

--- frontend/src/components/home/FeaturedSection.tsx
+++ frontend/src/components/home/FeaturedSection.tsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react';
+import { getComedyMovies } from '../../api/RecommenderAPI';
 import MoviePoster from '../movieCards/MoviePoster';
+import { Movie } from '../../types/Movie';
 
 function FeaturedSection() {
-  const featuredMovies = [
-    { imageUrl: 'https://example.com/movie1.jpg', title: 'Movie One' },
-    { imageUrl: 'https://example.com/movie2.jpg', title: 'Movie Two' },
-    { imageUrl: 'https://example.com/movie3.jpg', title: 'Movie Three' },
-    { imageUrl: 'https://example.com/movie4.jpg', title: 'Movie Four' },
-  ];
+  const [ComedyMovieData, setComedyMovies] = useState<Movie[]>([])
+  useEffect(() => {
+    const fetchComedyMovies = async () => {
+      try {
+        const result = await getComedyMovies();
+        if (result) {
+          setComedyMovies(result);
+        }
+      } catch (err) {
+        console.error('Failed to fetch comedy movies:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchComedyMovies();
+  }, []);
+
+
+
+
 
   return (
     <section className="featured-section">
       <h2>Featured on MyFlix</h2>
       <div className="featured-movie-grid">
-        {featuredMovies.map((movie, index) => (
+        {ComedyMovieData.map((movie, index) => (
           <div key={index} className="featured-movie-card">
             <MoviePoster imageUrl={movie.imageUrl} title={movie.title} />
           </div>
@@ -23,3 +40,11 @@ function FeaturedSection() {
 }
 
 export default FeaturedSection;
+function setMovies(result: Movie[]) {
+  throw new Error('Function not implemented.');
+}
+
+function setLoading(arg0: boolean) {
+  throw new Error('Function not implemented.');
+}
+
