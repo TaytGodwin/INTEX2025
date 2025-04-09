662190cd907d5a581e3bf1be725abb13099e5d20
masondalton <mcoled@byu.edu>
2025-04-09 13:57:13 -0600

    Stuff

--- backend/INTEX2025.API/Data/ApplicationDbContext.cs
+++ backend/INTEX2025.API/Data/ApplicationDbContext.cs
@@ -6,19 +6,17 @@ namespace INTEX.API.Data
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public object Movies_User;
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
         // DbSet for your custom User model
         public DbSet<movies_user> Movies_Users { get; set; }
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder); // Important to call this if you're inheriting from IdentityDbContext
-
+            base.OnModelCreating(modelBuilder); // keep this
+            modelBuilder.Entity<movies_user>().ToTable("movies_users");
         }
     }
 }
