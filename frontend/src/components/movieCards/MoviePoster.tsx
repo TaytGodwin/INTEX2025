3b2b8ed04c1819381024a42ca75b88ec7bfffb7c
IcyThunder22 <153141281+IcyThunder22@users.noreply.github.com>
2025-04-09 17:42:34 -0600

    start editing poster

--- frontend/src/components/movieCards/MoviePoster.tsx
+++ frontend/src/components/movieCards/MoviePoster.tsx
@@ -1,16 +1,15 @@
-// This is a component for the Movie posters for carousels, including minimum details. 
-
 import React from 'react';
 import '../../css/theme.css'; // Update or create a CSS file for styling
 
 interface MoviePosterProps {
   imageUrl: string;
   title: string;
+  onClick: () => void; // Callback function to open the modal with movie details
 }
 
-const MoviePoster: React.FC<MoviePosterProps> = ({ imageUrl, title }) => {
+const MoviePoster: React.FC<MoviePosterProps> = ({ imageUrl, title, onClick }) => {
   return (
-    <div className="movie-poster">
+    <div className="movie-poster" onClick={onClick} style={{ cursor: 'pointer' }}>
       <div className="poster-image-container">
         <img src={imageUrl} alt={title} className="poster-image" />
       </div>
