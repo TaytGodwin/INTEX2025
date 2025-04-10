2b33996cc5e3da8f9c6de92db495e1ecf0273ac2
IcyThunder22 <153141281+IcyThunder22@users.noreply.github.com>
2025-04-10 14:01:40 -0600

    please let home be right

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import LandingHero from '../components/home/LandingHero';
-import FeaturedSection from '../components/home/FeaturedSection';
+// import FeaturedSection from '../components/home/FeaturedSection';
 import TestimonialsSection from '../components/home/TestimonialsSection';
 import FAQSection from '../components/home/FAQSection';
-import PricingSection from '../components/home/PricingSection'; // Import PricingSection
+import PricingSection from '../components/home/PricingSection';
 import { Link } from 'react-router-dom';
 import CookiesModal from '../components/home/CookiesModal';
 import { useEffect, useState } from 'react';
 
 // This is the page that doesn't have info
+
 function HomePage() {
   const [shouldShow, setShouldShow] = useState(false);
   const [showCookiesModal, setShowCookieModal] = useState(true);
@@ -19,32 +20,6 @@ function HomePage() {
       setShouldShow(true);
     }
   }, []);
-
-  // Pricing data to pass as props to PricingSection
-  const pricingData = [
-    {
-      title: 'Basic Plan',
-      price: 9.99,
-      features: ['Feature 1', 'Feature 2', 'Feature 3'],
-      buttonText: 'Get Started',
-      planId: 'basic-plan',
-    },
-    {
-      title: 'Pro Plan',
-      price: 19.99,
-      features: ['Feature A', 'Feature B', 'Feature C'],
-      buttonText: 'Get Started',
-      planId: 'pro-plan',
-    },
-    {
-      title: 'Premium Plan',
-      price: 29.99,
-      features: ['Feature X', 'Feature Y', 'Feature Z'],
-      buttonText: 'Get Started',
-      planId: 'premium-plan',
-    },
-  ];
-
   return (
     <div>
       {shouldShow && showCookiesModal && (
@@ -53,11 +28,7 @@ function HomePage() {
       <LandingHero />
       <TestimonialsSection />
       <FAQSection />
-
-      {/* Pass the pricing data to PricingSection */}
-      
-
-    
+      <PricingSection />
     </div>
   );
 }
