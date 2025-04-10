6eceef88cc3a8aa66001f168475392e85a3afa21
Mekeli Hiller <mekelihiller@gmail.com>
2025-04-09 18:25:29 -0600

    Netflix type

--- frontend/src/components/Carousels/GenreRec.tsx
+++ frontend/src/components/Carousels/GenreRec.tsx
@@ -87,10 +87,10 @@ const GenreRec: React.FC<GenreRecProps> = ({genre}) => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 4, slidesToScroll: 4 } },
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 4 } },
       { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 2 } },
     ],
   };
