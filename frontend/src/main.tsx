acd2da5fdbcec648d59f0c40d2690926c0577a22
masondalton <mcoled@byu.edu>
2025-04-10 07:56:54 -0600

    Clone Point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/theme.css';
 import App from './App.tsx';
-
+// Clone point
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
