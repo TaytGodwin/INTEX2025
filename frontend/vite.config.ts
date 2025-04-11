1f4eaa28b498e6267318314257ed43aa3cf09785
masondalton <mcoled@byu.edu>
2025-04-10 21:41:47 -0600

    Update vite.config for hosted url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
         "img-src 'self' data: blob:; " +
         "frame-ancestors 'none'; " +
         "font-src 'self' fonts.gstatic.com data:; " +
-        "connect-src 'self' https://localhost:5000 https://cinenichebackend-fjhdf8csetdbdmbv.westus2-01.azurewebsites.net'; " +
+        "connect-src 'self' https://localhost:5000 https://cinenichebackend-fjhdf8csetdbdmbv.westus2-01.azurewebsites.net; " +
         "object-src 'none'; " +
         "base-uri 'self'; " +
         "form-action 'self';",
